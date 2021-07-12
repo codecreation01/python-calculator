7ec9f7e0b489978a4da27922a61651fe4d394c8f
codecreation01 <86874240+codecreation01@users.noreply.github.com>
2021-07-12 11:01:57 +0530

    created calculator.py
    demonstartes the working of an calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+num1=int(input("enter the first number))
+num2= int(input("enter second number))
+op=input("enter operation")
+if op=="+"
+     print(num1+num2)
+elif op=="-"
+     print(num1-num2)
+elif op=="*"
+     print(num1*num2)
+elif op=="/"
+     print(num1/num2)
+else
+     print("enter valid operation")
+                
+                
